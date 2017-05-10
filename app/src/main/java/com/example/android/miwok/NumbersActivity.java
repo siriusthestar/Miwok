06721de9e9cf7ea5a76c1528e0377e42dc99ff3b
siriusthestar <nevena.grubelic@gmail.com>
2017-05-10 22:07:16 +0200

    update

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,26 +2,19 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.concurrent.locks.Condition;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
-        // Create an array of words
+        // Create a list of words
         ArrayList<Word> words = new ArrayList<Word>();
-
         words.add(new Word("one", "lutti"));
         words.add(new Word("two", "otiiko"));
         words.add(new Word("three", "tolookosu"));
@@ -33,23 +26,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("nine", "wo'e"));
         words.add(new Word("ten", "na'aacha"));
 
-        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
-        // adapter knows how to create layouts for each item in the list, using the
-        // simple_list_item_1.xml layout resource defined in the Android framework.
-        // This list item layout contains a single {@link TextView}, which the adapter will set to
-        // display a single word.
+        // Create an {@link ArrayAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
         WordAdapter adapter =
                 new WordAdapter(this, words);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // word_list.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each word in the list of words.
-        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,11 +1,13 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,29 +18,51 @@ import java.util.ArrayList;
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+    }
+
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        //return super.getView(position, convertView, parent);
-        View customListItem = convertView;
-        if (customListItem==null) {
-            customListItem= LayoutInflater.from(getContext()).inflate(
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
 
-        Word wordToDisplay = getItem(position);
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
 
-        TextView miwokTextView = (TextView) customListItem.findViewById(R.id.miwok_text_view);
-        TextView defaultTextView = (TextView) customListItem.findViewById(R.id.default_text_view);
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
 
-        miwokTextView.setText(wordToDisplay.getMiwokTranslation());
-        defaultTextView.setText(wordToDisplay.getDefaultTranslation());
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
 
-        return customListItem;
-    }
+        /**
+         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+         ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+         // Get the image resource ID from the current AndroidFlavor object and
+         // set the image to iconView
+         iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+         */
 
-    public WordAdapter (NumbersActivity numbersActivity, ArrayList<Word> words) {
-        super(numbersActivity, 0, words);
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
     }
 
 }
